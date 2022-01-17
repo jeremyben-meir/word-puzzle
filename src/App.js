565f8fcdbac9da3ffc280b2cddf47e0c32c75cdf
jeremyben-meir <jsb459@cornell.edu>
2022-01-17 06:57:40 -0500

    google analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Game from "./views/Game2"
 import * as Styles from "./assets/Styles";
+import ReactGa from 'react-ga';
+import React, {useEffect} from 'react';
 
 function App() {
   const wordlen = 5;
   const numtries = 6; 
+
+  useEffect(() => {
+    ReactGa.initialize('UA-217441496-1')
+    ReactGa.pageview("/word-puzzle")
+  }, [])
 
   return (
     <div style={Styles.mainMainDiv}>
